40b12b01592143a7ee9e4d0dbf1ce4d0dee9ea00
Robert Burns <robertcburns25@gmail.com>
2022-12-06 02:11:02 -0500

    Added save feature and fixed bugs

--- app/src/main/java/com/example/ia08/MainActivity.java
+++ app/src/main/java/com/example/ia08/MainActivity.java
@@ -5,13 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity  {
 
@@ -22,8 +28,7 @@ public class MainActivity extends AppCompatActivity  {
     private Bitmap bitmap;
     private Canvas canvas;
     private Paint paint =  new Paint();
-    private int opacity;
-
+    private int opacity = 255;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,7 +45,7 @@ public class MainActivity extends AppCompatActivity  {
             public void onProgressChanged(SeekBar seekBar, int progress,
                                           boolean fromUser) {
                 // TODO Auto-generated method stub
-                opacity = progress;
+                opacity = (int) (255 * (progress * 0.01));
             }
 
             @Override
@@ -57,7 +62,7 @@ public class MainActivity extends AppCompatActivity  {
 
     }
 
-    private void Draw(){
+    private void Draw() {
         if (bitmap == null) {
             bitmap = Bitmap.createBitmap(imageView.getWidth(),
                     imageView.getHeight(),
@@ -101,39 +106,62 @@ public class MainActivity extends AppCompatActivity  {
         return super.onTouchEvent(event);
     }
 
+    public void resetOpacity() {
+        SeekBar sb = (SeekBar)findViewById(R.id.seekBar);
+        sb.setProgress(100);
+        opacity = 255;
+        paint.setAlpha(opacity);
+    }
     public void setColorRed(View view) {
         paint.setColor(Color.RED);
+        resetOpacity();
     }
 
     public void setColorGreen(View view) {
         paint.setColor(Color.GREEN);
+        resetOpacity();
     }
 
     public void setColorBlue(View view) {
         paint.setColor(Color.BLUE);
+        resetOpacity();
     }
 
     public void setColorBlack(View view) {
         paint.setColor(Color.BLACK);
+        resetOpacity();
     }
 
-    public void setBrushSmall(View view) {
-        paint.setStrokeWidth(4);
-    }
+    public void setBrushSmall(View view) { paint.setStrokeWidth(4); }
 
-    public void setBrushReg(View view) {
-        paint.setStrokeWidth(8);
-    }
+    public void setBrushReg(View view) { paint.setStrokeWidth(8); }
 
-    public void setBrushLarge(View view) {
-        paint.setStrokeWidth(16);
-    }
+    public void setBrushLarge(View view) {paint.setStrokeWidth(16); }
 
-    public void setBrushXL(View view) {
-        paint.setStrokeWidth(32);
-    }
+    public void setBrushXL(View view) { paint.setStrokeWidth(32);}
 
     public void clearCanvas(View view) {
     bitmap.eraseColor(Color.TRANSPARENT);
+    }
+
+    public void saveImageButton(View view) {
+        File savedImage = new File(this.getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+                Calendar.getInstance().getTime().toString() + ".jpg");
+
+        try {
+
+            FileOutputStream fileOutputStream = new FileOutputStream(savedImage);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream);
+            fileOutputStream.flush();
+            fileOutputStream.close();
+            MediaStore.Images.Media.insertImage(getContentResolver()
+                    ,savedImage.getAbsolutePath(),savedImage.getName(),savedImage.getName());
+            Toast.makeText(this,
+                    "File Saved Successfully",
+                    Toast.LENGTH_LONG).show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
